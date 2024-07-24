113ea55076fc0a0b0254d2b6cab3e9c7caa3e1a8
JesusGarciaSantiago <jesusgs0729@gmail.com>
2024-07-24 17:26:32 -0600

    Almost all routes

--- routes/admin.js
+++ routes/admin.js
@@ -6,7 +6,7 @@ const jwt = require('jsonwebtoken');
 
 
 
-admin.post ("/login", async (req, res, next) =>{
+admin.post ('/login', async (req, res, next) =>{
     const {mail, user_pass} = req.body;
     const query = `SELECT * FROM admin WHERE mail = '${mail}' AND user_pass = '${user_pass}'`;
     const rows = await db.query (query);
@@ -27,5 +27,59 @@ admin.post ("/login", async (req, res, next) =>{
     }
     return res.status(500).json({code: 500, message: "Campos incompletos"});
 });
+
+
+admin.post('/new-employee', async (req, res, next)=>{
+    const {name, last_name, phone, mail, address} = req.body;
+    if(name && last_name && phone && mail && address){
+        let query = `INSERT INTO employees (name, last_name, phone, mail, address)`;
+        query += `VALUES ('${name}', '${last_name}', '${phone}','${mail}', '${address}');`;
+
+        const rows = await db.query(query);
+        
+        if (rows.affectedRows == 1){
+            return res.status(201).json({code: 201, message: "Usuario registrado correctamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ha ocurrido un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+
+
+
+admin.delete("/:id([0-9]{1,3})", async (req, res, next)=>{
+    const query = `DELETE FROM employees WHERE employee_id = ${req.params.id} `;
+    const rows = await db.query(query);
+
+    
+        if(rows.affectedRows == 1){
+            return res.status(200).json({code:200, message:"Empleado borrado correctamente"});
+    
+        }
+
+    
+    return res.status(404).json({code:404, message:"Empleado no encontrado"});
+
+
+});
+
+
+admin.put("/:id([0-9]{1,3})", async(req, res, next)=>{
+
+    const {name, last_name, phone, mail, address} = req.body;
+    if(name && last_name && phone && mail && address){
+        let query = `UPDATE employees SET name='${name}', last_name ='${last_name}',`;
+        query += `phone = '${phone}', mail= '${mail}', address = '${address}'`;
+
+        const rows = await db.query(query);
+
+        if(rows.affectedRows == 1){
+            return res.status(200).json({code: 200, message:"Empleado actualizado"});
+        }
+        return res.status(500).json({code: 500, message:"Ocurrió un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+
 
 module.exports = admin;
